936d1e9a3bef8969ce672205365eed1cf0be56f9
HayashiRoj <2034502495@qq.com>
2025-10-10 17:21:17 +0800

    biggg update(not big))

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # yechik.github.io
+** 网址：https://HayashiRoj.github.io/yechik.github.io
+** 美观的网址：https://yechik.moe.page
---
+++ search_script.js
@@ -0,0 +1,4 @@
+function goSearch() {
+    var inputVal = document.getElementById('searchBox').value;
+    window.location.herf("https://cn.bing.com/search?q="+inputVal);
+}
